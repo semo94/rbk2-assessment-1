90b59f7b62d3fa1ca81a8f9cc363f4471d1e48be
semo94 <saleem.bakri94@gmail.com>
2017-01-07 09:31:34 +0200

    ds-stack solution

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,16 @@
 var Stack = function() {
-	//your code is here
+	var stack = Object.creat(prototypeMethodes);
+	stack._storage = [];
+	return stack;
+};
+
+var prototypeMethodes = {};
+prototypeMethodes.add = function(value){
+	this._storage.push(value);
+
+};
+prototypeMethodes.remove = function(){
+	if(this._storage.length > 0){
+		return this._storage.pop();
+	}
 };
